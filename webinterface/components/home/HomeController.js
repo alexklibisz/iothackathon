2f923b5405df7bc6c6a7dba12c29e8bbaf0b5843
Alex Klibisz <aklibisz@vols.utk.edu>
2015-04-26 15:38:17 -0700

    alert on update

--- webinterface/components/home/HomeController.js
+++ webinterface/components/home/HomeController.js
@@ -2,6 +2,7 @@ app.controller('HomeController',
     function($scope, $timeout, $http) {
 
         $scope.updated = false;
+        $scope.secondsAgo = 0;
 
         $scope.person = {
             name: 'Jane Doe'
@@ -17,8 +18,7 @@ app.controller('HomeController',
                 $http.get('http://localhost:8000/data.json').success(function(data) {
                     $scope.data.meds = parseData(data);
                 })
-                $timeout(get, 1000);
-                // console.log("updated");
+                $timeout(get, 2000);
             }
             $timeout(get, 0);
         }
@@ -35,24 +35,34 @@ app.controller('HomeController',
                 weightPerPill = 30,
                 pressureThreshold = 200,
                 pillsPerDay = 2;
-                
+
+            $scope.updated = false;
 
             for (var i = 0; i < data.length; i++) {
                 var source = data[i],
-                    target = {}, tmp;
+                    target, tmp;
+
+                if ($scope.data.meds.length === 0) {
+                    target = {};
+                } else {
+                    target = $scope.data.meds[i];
+                }
 
                 // get the name, just in case it changed
                 target.name = source.name;
-                
+
                 // convert the weight to pillsRemaining
                 if (target.weight === undefined) target.weight = source.weight;
                 tmp = target.pillsRemaining;
                 target.pillsRemaining = Math.floor(target.weight / weightPerPill);
-                if(tmp != target.pillsRemaining) console.log('pr changed');
+                if (tmp != target.pillsRemaining) $scope.updated = true;
+
                 // check the pressure to update the timeTaken
-                if(source.pressure < pressureThreshold) {
+                if (source.pressure < pressureThreshold
+                    && target.pressure !== source.pressure) {
                     target.pressure = source.pressure;
                     target.timeTaken = new Date();
+                    $scope.updated = true;
                 }
 
                 // update the estimatedRefill
@@ -60,11 +70,17 @@ app.controller('HomeController',
                 target.estimatedRefill = new Date();
                 target.estimatedRefill.setDate((new Date()).getDate() + daysRemaining);
 
+                if($scope.updated) {
+                    alert("Updated " + target.name);
+                }
+
                 updated[i] = target;
             }
-            console.log('returning', JSON.stringify(updated));
+       
             return updated;
+
         }
+
 
         poll();
 
